6f22798b52faa3983493a21c8815ea055d5a85c9
Phyleria <atienophyllis032@gmail.com>
2022-05-28 14:10:07 +0300

    .

---
+++ asset/js/script.js
@@ -0,0 +1,11 @@
+document.querySelector('.header__moon').onclick = function() {
+    document.querySelector('body').classList.toggle('is-black');
+}
+
+document.querySelector('.header__mob-menu').onclick = function() {
+    document.querySelector('.header-menu').classList.toggle('is-active');
+}
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }

bdfd364ad66d659e148fcfee5ec8c892cca57b5d
Phyllis Atieno <atienophyllis032@gmail.com>
2022-06-07 22:40:37 +0300

    .

--- asset/js/script.js
+++ asset/js/script.js
@@ -9,3 +9,35 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
+
+const burger = document.getElementById('burger');
+const ul = document.querySelector('nav ul');
+const homeAnchor = document.getElementById('home-anchor');
+const stackAnchor = document.getElementById('stack-anchor');
+const commAnchor = document.getElementById('comm-anchor');
+const eduAnchor = document.getElementById('edu-anchor');
+const expAnchor = document.getElementById('exp-anchor');
+const projAnchor = document.getElementById('proj-anchor');
+
+burger.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+
+homeAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+stackAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+commAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+eduAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+expAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+projAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})

317bdbc6c2e4e972fe0525d7e46c3d4a63d97162
Phyllis Atieno <atienophyllis032@gmail.com>
2022-06-14 20:04:25 +0300

    .

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,5 @@
-document.querySelector('.header__moon').onclick = function() {
-    document.querySelector('body').classList.toggle('is-black');
+document.querySelector('.header__moon').onclick = function() {  //Returns the header_moon class element and specifies it to perform a function when the button is clicked
+    document.querySelector('body').classList.toggle('is-black'); //Returns the element body and toggles it according to the "is-black" element specifications
 }
 
 document.querySelector('.header__mob-menu').onclick = function() {
